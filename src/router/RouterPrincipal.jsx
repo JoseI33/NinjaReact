360c92a2b787f5fe8c912376f9aca27964a1230a
Josei33 <jose.imhoff.20@gmail.com>
2023-07-25 23:21:07 -0300

    Funcion de la pagina y routeando los componentes

--- src/router/RouterPrincipal.jsx
+++ src/router/RouterPrincipal.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route, } from 'react-router-dom'
-import {Home} from '../assets/components/Pages/Home'
-import {Footer} from '../assets/components/Pages/Footer/Footer'
-import {Error} from '../assets/components/Pages/Error404/Error'
+import Home from '../Pages/Home/Home'
+import Footer from '../Pages/Footer/Footer'
+import Error from '../Pages/Error/Error'
 
 function RouterPrincipal() {
     return (
         <BrowserRouter>
             <Routes>
-
-                <Route path='/' element={Home}/>
-                <Route path='/' element={Footer}/>
-                <Route path='/' element={Error}/>
+                <Route path='/' element={<Home/>} />
+                <Route path='/footer' element={<Footer/>} />
+                <Route path='*' element={<Error/>} />
             </Routes>
         </BrowserRouter>
     )
