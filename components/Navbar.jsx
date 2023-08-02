18bb138bec736694ba7d111377b75dbe4ee1957c
Josei33 <jose.imhoff.20@gmail.com>
2023-08-01 21:37:14 -0300

    Finalizado prueba de creacion de proyecto React.

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,27 +3,25 @@ import { Button } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { BrowserRouter as Routes, Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 
 
 function BasicExample() {
-    return ( 
-        <Routes>
+    return (     
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
                 <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link as={Link} to={"/"}>Home</Nav.Link>
-                        <Nav.Link as={Link} to={"/footer"}>Footer</Nav.Link>
+                        <NavLink className="nav-link" to={"/"}>Home</NavLink>
+                        <NavLink className="nav-link" to={"/footer"}>Footer</NavLink>
                     </Nav>
                 </Navbar.Collapse>
                 <Button className='btn btn-dark'>Submit</Button>
             </Container>
         </Navbar>
-        </Routes>
     );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import RouterPrincipal from './router/RouterPrincipal.jsx'
-import Navbar from '../components/Navbar.jsx'
 
 //bootstrap.
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Navbar/>
-    <RouterPrincipal/>
     <App />
   </React.StrictMode>,
 )
